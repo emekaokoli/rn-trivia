4140326f7537a3de97172d03215a8bcb12fe6a99
Emeka Okoli <emekaokoli00@gmail.com>
2022-04-24 23:28:24 +0100

    inittial commit

---
+++ components/QuestionList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Proptypes from 'proptypes';
+import { useSelector } from 'react-redux';
+import { List ,Colors } from 'react-native-paper';
+
+List
+
+export default function QuestionList() {
+  const { questionsReponse } = useSelector((state) => ({
+    questionsReponse: state.questionsReponse,
+  }));
+
+  const questionlist = questionsReponse?.map((data) => {
+    const { question, correct_answer } = data.Question;
+    const { answer, id } = data;
+    return (
+      <List.Section className='question' key={id}>
+        <br />
+        {correct_answer === answer ? (
+          <List.Item
+            title={question}
+            //description={`Correct Answer: ${correct_answer}`}
+            left={() => (
+              <List.Icon icon='check' color={Colors.green500} />
+            )}
+          />
+        ) : (
+          <List.Item
+            title={question}
+            //description={`Correct Answer: ${correct_answer}`}
+            left={() => (
+              <List.Icon icon='close' color={Colors.red500} />
+            )}
+          />
+        )}
+        {/* {question} */}
+      </List.Section>
+    );
+  });
+  return <>{questionlist}</>;
+}
+QuestionList.propTypes = {
+  questionsReponse: Proptypes.arrayOf(
+    Proptypes.objectOf(Proptypes.string)
+  ),
+};
